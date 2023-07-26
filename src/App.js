c4ffa29954683103eabf80ef3bb328cbc4079b2d
ansel brandt <ansel@anselbrandt.com>
2023-07-25 22:14:25 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Scheduler from "./scheduler/Scheduler";
+
+const App = () => {
+  return (
+    <Scheduler/>
+  );
+}
+
+export default App;
+
